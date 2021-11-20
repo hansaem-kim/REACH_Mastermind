4e74343bdf6bd90ab2cb2609eb876983b66f730d
Hansaem Kim <hansaem_kim@hotmail.com>
2021-11-19 22:50:33 -0500

    Create Game Board

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import Game from "./scripts/game.js";
 import Modal from "./scripts/modal.js"
 
 document.addEventListener("DOMContentLoaded", ()=>{
     const modal = new Modal();
+    const game = new Game();
 
     const startButton = document.querySelector("#start-btn");
+    game.generatePattern();
     startButton.onclick = function() {
         modal.closeModal("welcome-modal");
     }
---
+++ src/scripts/game.js
@@ -0,0 +1,22 @@
+class Game {
+    constructor(){
+        this.pattern = [];
+    }
+
+    generatePattern(){
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", 'https://www.random.org/integers/?num=4&min=1&max=8&col=1&base=10&format=plain&rnd=new');
+        xmlHttp.send();
+        xmlHttp.onreadystatechange=(e=>{
+            let stringPattern = xmlHttp.responseText;
+            this.pattern = stringPattern.match(/[^\s]+/g);
+        });
+
+        // for(let i=0; i<4; i++){
+        //     this.pattern[i] = Math.floor(Math.random()*7)+1;
+        // }
+        // console.log(this.pattern);
+    }
+}
+
+export default Game;
